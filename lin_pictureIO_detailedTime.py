9773e73cc7a6314deaa4ed38c5c5fd02695a9551
Cathy52 <39331694+Cathy52@users.noreply.github.com>
2020-12-29 14:48:45 +0800

    Update lin_pictureIO_detailedTime.py

--- lin_pictureIO_detailedTime.py
+++ lin_pictureIO_detailedTime.py
@@ -52,65 +52,55 @@ if __name__ == "__main__":
         parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
         parser.add_argument("--img_size", type=int, default=608, help="size of each image dimension")
         opt = parser.parse_args()
-        print("------")
+        print("------开始时间")
         print(time.time())
-
+        # 判断：忽略时间
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(time.time())
-
         os.makedirs("output", exist_ok=True)
-        # 读取所有的类别
+        # 读取所有的类别：读取文件-忽略时间
         classes = load_classes(opt.class_path)  # Extracts class labels from file
-        print("------")
-        print(time.time())
-        # 构建了一个Float型的张量
+        # 构建了一个Float型的张量：忽略时间
         Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-        print("--模型检测----")
-        print(time.time())
-
+    
+    # 1.检测模型构建
+    print("------检测：使用Draknet构建检测模型+加载权重文件")
+    print(time.time())
     # 定义检测用的模型
     if(True):
         # Set up model # 加载权重文件 # 不启用 BatchNormalization 和 Dropout
         # 深度学习框架 Darknet 用C和CUDA编写的开源神经网络框架
         model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
-        print("------")
+        print("构建模型")
         print(time.time())
         model.load_state_dict(torch.load(opt.weights_path))
-        print("------")
+        print("加载权重文件")
         print(time.time())
         model.eval()  # Set in evaluation mode 
-        print("---模型识别---")
-        print(time.time())
+    print("检测结束")
+    print(time.time())
 
+    # 1.识别模型构建
+    print("------识别：ResNet18初始化+load_state_dict+to(gpu)+eval")
+    print(time.time())
     # 定义分类用的模型
     if(True):
         # model_class = FashionCNN(sign_classes)
         model_class = ResNet18(sign_classes)
-        print("------")
-        print(time.time())
         model_class.load_state_dict(torch.load(classes_weights_path))
-        print("------")
-        print(time.time())
         model_class.to(device)
-        print("------")
-        print(time.time())
         model_class.eval()
+    print("识别模型构建结束")
+    print(time.time())
 
-    crop_dirs = ["image_for_detect/Tinghua100K"]
-    
-    # 每一张图片进行识别
+    crop_dirs = ["image_for_detect/Tinghua100K"]    
     for dir_ in crop_dirs:
-        print("---开始---")
-        print(time.time())
         train_results = {"imgs" : {}}
-
         opt.image_folder = dir_
-
-        # for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
         names = os.listdir(opt.image_folder)
         nums = 0
         for name in tqdm(names[:]):
-            print("----图片开始--")
+            # 3.处理图片
+            print("------处理图片：读取转换为tensor-0.4，resize-0，")
             print(time.time())
 
             img_id = name.split(".")[0]
@@ -123,28 +113,32 @@ if __name__ == "__main__":
 
             # Extract image as PyTorch tensor
             img = torchvision.transforms.ToTensor()(Image.open(img_path).convert(mode="RGB"))
-            print("--读取完毕，转换成了tensor变量----")
+            print("读取转换成tensor变量")
             print(time.time())
           
             input_imgs, _ = pad_to_square(img, 0)
-            # Resize
+            # Resize：忽略时间
             input_imgs = resize(input_imgs, opt.img_size).unsqueeze(0)
 
             # Configure input
             input_imgs = Variable(input_imgs.type(Tensor))
-            print("---开始检测---")
+            print("读取，处理图片完毕")
             print(time.time())
 
+            # 4.检测图片
+            print("------检测图片")
+            print(time.time())
             # Get detections
             with torch.no_grad():
                 detections = model(input_imgs.to(device))
                 detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)[0]
 
-            print("---检测完毕---")
+            print("检测完毕")
             print(time.time())
 
             if  detections is not None:  #  one image
-                print("---检测结果不为空---")
+                # 5.识别
+                print("------识别图片：前提为检测结果不为空")
                 print(time.time())
 
                 objects = []  #  save the results of a image detection
@@ -154,12 +148,10 @@ if __name__ == "__main__":
                 img_copy =Image.open(img_path) 
                 j = 0
                 
-                print("------")
+                print("简单处理，提取该图片每一个检测框内的内容")
                 print(time.time())
 
                 for i, (x1, y1, x2, y2, conf, cls_conf, cls_pred) in enumerate(detections): #  one object in a image                
-                    print("---开始分析一张图片的每一个检测的区域---")
-                    print(time.time())
                     x1 = int(x1)
                     y1 = int(y1)
                     x2 = int(x2)
@@ -169,8 +161,9 @@ if __name__ == "__main__":
                     
                     min_sign_size = 10
 
+                    # 不处理太小的检测框
                     if box_w >= min_sign_size and box_h >= min_sign_size:                        
-                        print("--如果区域足够大，切割图片，做一种处理----")
+                        print("依次分析每一个检测框")
                         print(time.time())
                         crop_sign_org = img_copy.crop((x1, y1, x2, y2)).convert(mode="RGB")
 
@@ -183,37 +176,35 @@ if __name__ == "__main__":
 
                         crop_sign_input = test_transform(crop_sign_org).unsqueeze(0)
 
-                        print("---处理好了---")
+                        print("简单处理处理")
                         print(time.time())
 
                         with torch.no_grad():
-                            print("---开始识别---")
+                            print("开始识别")
                             print(time.time())
                             pred_class = model_class(crop_sign_input.to(device))
-                            print("---识别结束---")
+                            print("识别结束")
                             print(time.time())
                         sign_type  = torch.max(pred_class, 1)[1].to("cpu").numpy()[0]
                         # #### to class  ###############
                         cls_pred = sign_type
 
-                        print("cls_pred_type = ", classes[int(cls_pred)])
-
-                        print("---以下：开始保存切割后的图片---")
-                        print(time.time())
-                        # #############
-                        # save crop image
-                        # #############                        
-                        if classes[int(cls_pred)] != "zo":
-                            # save  crop image #############
-                            save_dir = "img_crop_2_classification_Tinghua_weights_11/" + classes[int(cls_pred)]
-                            if not os.path.exists(save_dir):
-                                os.makedirs(save_dir)
-                            name = dir_.split("/")[-2] + "_" + dir_.split("/")[-1] + str(int(random.random() * 100000000))
-                            print("save path:", save_dir, str(name) + ".jpg")
-                            #  save crop sign
-                            crop_sign_org.save(os.path.join(save_dir, str(name) + ".jpg"))
+                        # print("---以下：开始保存切割后的图片---")
+                        # print(time.time())
+                        # # #############
+                        # # save crop image
+                        # # #############                        
+                        # if classes[int(cls_pred)] != "zo":
+                        #     # save  crop image #############
+                        #     save_dir = "img_crop_2_classification_Tinghua_weights_11/" + classes[int(cls_pred)]
+                        #     if not os.path.exists(save_dir):
+                        #         os.makedirs(save_dir)
+                        #     name = dir_.split("/")[-2] + "_" + dir_.split("/")[-1] + str(int(random.random() * 100000000))
+                        #     print("save path:", save_dir, str(name) + ".jpg")
+                        #     #  save crop sign
+                        #     crop_sign_org.save(os.path.join(save_dir, str(name) + ".jpg"))
                         
-                        print("--保存结束，开始标记----")
+                        print("开始标记图片")
                         print(time.time())
                        
                         if True and classes[int(cls_pred)] != "zo":     
@@ -246,7 +237,7 @@ if __name__ == "__main__":
                             
                             #  save predict results to a json file: my_train_results.json
                             objects.append({'category': classes[int(cls_pred)], 'score': 848.0, 'bbox': {'xmin': x1, 'ymin': y1, 'ymax': y2, 'xmax': x2}})
-                        print("--标记结束，开始保存图片----")
+                        print("该检测框标记结束")
                         print(time.time())
 
                 # Save generated image with detections
@@ -260,15 +251,18 @@ if __name__ == "__main__":
                     plt.savefig(f"output/{dir__ + str(nums).zfill(5)}.png", bbox_inches="tight", pad_inches=0.0,)
                 except:
                     continue
+                print("该图片识别，标记结束")
+                print(time.time())
             train_results["imgs"][img_id] = {"objects": objects}
-            print("--保存结束，开始写输出文本文件----")
+            print("保存此图片的结果")
             print(time.time())
-
+        print("------所有识别和检测结束，开始写输出文件")
+        print(time.time())
     # 将结果写到json文件中
     if(True):
         file_name = "output/Tinghua100K_result_for_test.json"
         with open(file_name, "w") as file_object:
             json.dump(train_results, file_object)
     
-    print("--结束 END----")
+    print("结束 END")
     print(time.time())
