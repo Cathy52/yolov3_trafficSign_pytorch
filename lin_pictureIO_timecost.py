e13ffcb667c008f4d3574fbcf5c621c577048bba
Cathy52 <39331694+Cathy52@users.noreply.github.com>
2020-12-31 15:17:51 +0800

    Update lin_pictureIO_timecost.py

--- lin_pictureIO_timecost.py
+++ lin_pictureIO_timecost.py
@@ -39,7 +39,6 @@ classes_weights_path = "ALL_sign_data/checkpoints/model_acc_97__class_115_epoch_
 
 
 if __name__ == "__main__":
-    # 准备：设置设备，所有分类，构建张量
     if(True):
         parser = argparse.ArgumentParser()
         parser.add_argument("--image_folder", type=str, default="data/changshu_18_during", help="path to dataset")
@@ -50,35 +49,33 @@ if __name__ == "__main__":
         parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
         parser.add_argument("--img_size", type=int, default=608, help="size of each image dimension")
         opt = parser.parse_args()
-        print("------开始时间")
-        t0 = time.time()
-        # 判断：忽略时间
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         os.makedirs("output", exist_ok=True)
-        # 读取所有的类别：读取文件-忽略时间
+        # 读取所有的类别：读取文件
         classes = load_classes(opt.class_path)  # Extracts class labels from file
-        # 构建了一个Float型的张量：忽略时间
+        # 构建了一个Float型的张量
         Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
     
-    # 1.检测模型构建
-    print("------1.检测模型：使用Draknet构建检测模型+加载权重文件")
+    print("--------1.加载检测模型：使用Draknet构建检测模型+加载权重文件")
     t1 = time.time()
     # 定义检测用的模型
     if(True):
         # Set up model # 加载权重文件 # 不启用 BatchNormalization 和 Dropout
         # 深度学习框架 Darknet 用C和CUDA编写的开源神经网络框架
         model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
-        print("构建模型")
-        print(time.time()-t1)
+        print("    构建模型")
+        t11 = time.time()
+        print(t11-t1)
         model.load_state_dict(torch.load(opt.weights_path))
-        print("加载权重文件")
-        print(time.time()-t1)
+        print("    加载权重文件")
+        t12 = time.time()
+        print(t12-t11)
         model.eval()  # Set in evaluation mode 
-    print("检测结束")
-    print(time.time()-t1)
+    print("    检测结束")
+    print(time.time()-t12)
 
     # 1.识别模型构建
-    print("------2.识别模型：ResNet18初始化+load_state_dict+to(gpu)+eval")
+    print("--------2.识别模型：ResNet18初始化+load_state_dict+to(gpu)+eval")
     t2 = time.time()
     # 定义分类用的模型
     if(True):
@@ -87,7 +84,7 @@ if __name__ == "__main__":
         model_class.load_state_dict(torch.load(classes_weights_path))
         model_class.to(device)
         model_class.eval()
-    print("识别模型构建结束")
+    print("    识别模型构建结束")
     print(time.time()-t2)
 
     crop_dirs = ["image_for_detect/Tinghua100K"]    
@@ -98,8 +95,8 @@ if __name__ == "__main__":
         nums = 0
         for name in tqdm(names[:]):
             # 3.处理图片
-            print("------------------------------------------------对每一张图片------------------------------------")
-            print("------3.检测前简单处理图片：读取转换为tensor-0.4，resize-0，")
+            print(name)
+            print("--------3.检测前简单处理图片：读取转换为tensor，resize-0，")
             t3 = time.time()
 
             img_id = name.split(".")[0]
@@ -112,32 +109,34 @@ if __name__ == "__main__":
 
             # Extract image as PyTorch tensor
             img = torchvision.transforms.ToTensor()(Image.open(img_path).convert(mode="RGB"))
-            print("读取转换成tensor变量")
-            print(time.time()-t3)
+            print("    读取转换成tensor变量")
+            t31 = time.time()
+            print(t31-t3)
           
             input_imgs, _ = pad_to_square(img, 0)
-            # Resize：忽略时间
+            # Resize
             input_imgs = resize(input_imgs, opt.img_size).unsqueeze(0)
 
             # Configure input
             input_imgs = Variable(input_imgs.type(Tensor))
-            print("读取，处理图片完毕")
-            print(time.time()-t3)
+            print("    读取，处理图片完毕")
+            t32 = time.time()
+            print(t32-t31)
 
             # 4.检测图片
-            print("------4.检测图片")
+            print("--------4.检测图片")
             t4 = time.time()
             # Get detections
             with torch.no_grad():
                 detections = model(input_imgs.to(device))
                 detections = non_max_suppression(detections, opt.conf_thres, opt.nms_thres)[0]
 
-            print("检测完毕")
+            print("    检测完毕")
             print(time.time()-t4)
 
             if  detections is not None:  #  one image
                 # 5.识别
-                print("------5.识别图片")
+                print("--------5.识别图片")
                 t5 = time.time()
 
                 objects = []  #  save the results of a image detection
@@ -147,8 +146,9 @@ if __name__ == "__main__":
                 img_copy =Image.open(img_path) 
                 j = 0
                 
-                print("识别前简单处理，提取图片所有的检测框内")
-                print(time.time()-t5)
+                print("    识别前处理：提取图片所有的检测框内")
+                t51 = time.time()
+                print(t51-t5)
 
                 for i, (x1, y1, x2, y2, conf, cls_conf, cls_pred) in enumerate(detections): #  one object in a image
                     t51 = time.time()
@@ -173,21 +173,24 @@ if __name__ == "__main__":
 
                         crop_sign_input = test_transform(crop_sign_org).unsqueeze(0)
 
-                        print("识别前对每一个检测框简单处理")
-                        print(time.time()-t51)
+                        print("    识别前对每一个检测框简单处理")
+                        t52 = time.time()
+                        print(t52-t51)
 
                         with torch.no_grad():
-                            print("开始识别")
-                            print(time.time()-t51)
+                            print("    开始识别")
+                            t53 = time.time()
+                            print(t53-t52)
                             pred_class = model_class(crop_sign_input.to(device))
-                            print("识别结束")
-                            print(time.time()-t51)
+                            print("    识别结束")
+                            t54 = time.time()
+                            print(t54-t53)
                         sign_type  = torch.max(pred_class, 1)[1].to("cpu").numpy()[0]
                         cls_pred = sign_type
                        
                         if True and classes[int(cls_pred)] != "zo":     
-                            print("识别后对每一个检测框进行标记")
-                            print(time.time()-t51)                            
+                            print("    识别后开始对每一个检测框进行标记")
+                            t55 = time.time()
                             color = "r"
                             bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=1, edgecolor=color, facecolor="none")
                             ax.add_patch(bbox)
@@ -219,8 +222,9 @@ if __name__ == "__main__":
                 # 对每一个检测框进行操作                
                     # 不处理太小的检测框
                         # 不标记某些检测框
-                            print("该检测框标记结束")
-                            print(time.time()-t51)
+                            print("    该检测框标记结束")
+                            t56 = time.time()
+                            print(t56-t55)
 
                 # Save generated image with detections
                 nums += 1
@@ -233,20 +237,16 @@ if __name__ == "__main__":
                     plt.savefig(f"output/{dir__ + str(nums).zfill(5)}.png", bbox_inches="tight", pad_inches=0.0,)
                 except:
                     continue
-            print("该图片识别，标记结束")
-            print(time.time()-t5)
             train_results["imgs"][img_id] = {"objects": objects}
-            print("保存此图片的结果")
+            print("    该图片保存结束")
+            print(time.time()-t56)
+            print("    该图片保存用时")
             print(time.time()-t3)
-        print("------6.所有识别和检测结束，开始写输出文件")
+            
+        print("--------6.所有识别和检测结束，开始写输出文件")
         t6 = time.time()
     # 将结果写到json文件中
     if(True):
         file_name = "output/Tinghua100K_result_for_test.json"
         with open(file_name, "w") as file_object:
             json.dump(train_results, file_object)
-    
-    print("结束 END")
-    print(time.time()-t6)
-    print("------一共时间")
-    print(time.time()-t0)
